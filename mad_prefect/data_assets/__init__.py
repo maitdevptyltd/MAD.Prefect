07964d1e18fd5aff1659a71fc0678f0614c79bd2
Rowan Farquharson <rowanfarquharson.aj@gmail.com>
2024-08-22 13:50:02 +1000

    feat(data_assets): Set up DataAssetArtifact and enhanced how artifact paths work.

--- mad_prefect/data_assets/__init__.py
+++ mad_prefect/data_assets/__init__.py
@@ -2,8 +2,13 @@ from typing import Callable
 from mad_prefect.data_assets.data_asset import DataAsset
 
 
-def asset(path: str, artifacts_dir: str | None = None, name: str | None = None):
+def asset(
+    path: str,
+    artifacts_dir: str | None = None,
+    name: str | None = None,
+    snapshot_artifacts: bool = True,
+):
     def decorator(fn: Callable):
-        return DataAsset(fn, path, artifacts_dir, name)
+        return DataAsset(fn, path, artifacts_dir, name, snapshot_artifacts)
 
     return decorator
--- mad_prefect/data_assets/data_asset.py
+++ mad_prefect/data_assets/data_asset.py
@@ -10,9 +10,85 @@ from mad_prefect.duckdb import register_mad_protocol
 import re
 import os
 
+ASSET_METADATA_LOCATION = os.getenv("ASSET_METADATA_LOCATION", ".asset_metadata")
+
+
+class DataAssetArtifact:
+    def __init__(
+        self,
+        artifact: object,
+        dir: str,
+        asset_id: str | None = None,
+        asset_run_id: str | None = None,
+        asset_name: str | None = None,
+        path: str | None = None,
+        data_written: bool = False,
+    ):
+        self.artifact = artifact
+        self.dir = dir
+        self.asset_id = asset_id
+        self.asset_run_id = asset_run_id
+        self.asset_name = asset_name
+        self.path = path
+        self.data_written = data_written
+        pass
+
+    def _get_params(self):
+        params = (
+            dict(self.artifact.request.url.params)
+            if isinstance(self.artifact, httpx.Response)
+            and self.artifact.request.url.params
+            else None
+        )
+        return params
+
+    def _get_base_path(self):
+        return self.dir
+
+    def _generate_artifact_guid(self):
+        hash_input = f"{self.asset_id}:{self.asset_run_id}:{self.path}"
+        return hashlib.md5(hash_input.encode()).hexdigest()
+
+    async def _write_artifact_metadata(self):
+        fs = await get_fs()
+        artifact_id = self._generate_artifact_guid()
+        artifact_metadata = {
+            "artifact_id": artifact_id,
+            "asset_run_id": self.asset_run_id,
+            "asset_id": self.asset_id,
+            "artifact_path": self.path,
+            "artifact_written": self.data_written,
+        }
+
+        await fs.write_data(
+            f"{ASSET_METADATA_LOCATION}/asset_name={self.asset_name}/asset_id={self.asset_id}/asset_run_id={self.asset_run_id}/_artifacts/artifact_id={artifact_id}/metadata.json",
+            artifact_metadata,
+        )
+
+    async def _run_artifact(
+        self, asset_id: str, asset_run_id: str, asset_name: str, artifact_path: str
+    ):
+        self.asset_id = asset_id
+        self.asset_run_id = asset_run_id
+        self.asset_name = asset_name
+        self.path = artifact_path
+
+        artifact_id = self._generate_artifact_guid
+        await self._write_artifact_metadata()
+
+        if _output_has_data(self.artifact):
+            try:
+                await _write_asset_data(self.path, self.artifact)
+                self.data_written
+            except:
+                raise ValueError(
+                    f"Artifact write operation failed for artifact_id: {artifact_id}"
+                )
+
+        await self._write_artifact_metadata()
+
 
 class DataAsset:
-    ASSET_METADATA_LOCATION = os.getenv("ASSET_METADATA_LOCATION", ".asset_metadata")
 
     def __init__(
         self,
@@ -20,11 +96,13 @@ class DataAsset:
         path: str,
         artifacts_dir: str | None = "",
         name: str | None = None,
+        snapshot_artifacts: bool = True,
     ):
         self.__fn = fn
         self.path = path
         self.artifacts_dir = artifacts_dir
         self.name = name if name else fn.__name__
+        self.snapshot_artifacts = snapshot_artifacts
         self.fn_name = fn.__name__
         self.fn_signature = inspect.signature(fn)
         self.runtime_str = None
@@ -112,57 +190,45 @@ class DataAsset:
             return None
 
     async def _handle_yield(self, *args, **kwargs):
-        artifact_glob_pattern = await self._create_yield_artifacts(*args, **kwargs)
-        await self._create_yield_output(artifact_glob_pattern)
+        artifact_glob = await self._create_yield_artifacts(*args, **kwargs)
+        await self._create_yield_output(artifact_glob)
 
     async def _create_yield_artifacts(self, *args, **kwargs):
         # Set up starting artifact fragment_number
         fragment_number = 1
-
-        # Extract artifact base path
-        base_path = await self._get_artifact_base_path()
+        artifact_glob: list[str] = []
 
         async for output in self.__fn(*args, **kwargs):
-            if self._output_has_data(output):
+            artifact = await self._handle_artifact(output, fragment_number)
 
-                params = (
-                    dict(output.request.url.params)
-                    if isinstance(output, httpx.Response) and output.request.url.params
-                    else None
-                )
-
-                path = self._build_artifact_path(base_path, params, fragment_number)
-                await self._write_operation(path, output)
-
+            if artifact.data_written:
                 print(
-                    f"An artifact for asset_id: {self.id} was written to path: \n {path}\n\n   "
+                    f"An artifact for asset_id: {self.id} was written to path: \n {artifact.path}\n\n   "
                 )
 
-                # Update fragment_number if used in path
-                fragment_number = (
-                    (fragment_number + 1) if "fragment=" in path else fragment_number
-                )
+            # Update fragment_number if data is written & it is used in path
+            if artifact.data_written and "fragment=" in str(artifact.path):
+                fragment_number = +1
 
-        artifact_glob_pattern = (
-            f"{base_path}/_artifacts/runtime={self.runtime_str}/**/*.json"
-        )
-        self.artifact_glob = artifact_glob_pattern
-        return artifact_glob_pattern
+            artifact_glob.append(f"mad://{artifact.path}")
 
-    async def _create_yield_output(self, glob_pattern: str):
+        self.artifact_glob = artifact_glob
+        return artifact_glob
+
+    async def _create_yield_output(self, artifact_glob: str):
         # Set up filesystem abstraction
         fs = await get_fs()
         await register_mad_protocol()
 
-        if fs.glob(glob_pattern):
+        if fs.glob(artifact_glob):
             duckdb.query("SET temp_directory = './.tmp/duckdb/'")
-            folder_path = await self._get_folder_path(self.resolved_path)
+            folder_path = os.path.dirname(self.resolved_path)
             fs.mkdirs(folder_path, exist_ok=True)
             duckdb.query(
                 f"""
                     COPY (
                     SELECT *
-                    FROM read_json_auto('mad://{glob_pattern}', hive_partitioning = true, union_by_name = true, maximum_object_size = 33554432)
+                    FROM read_json_auto({artifact_glob}, hive_partitioning = true, union_by_name = true, maximum_object_size = 33554432)
                     ) TO 'mad://{self.resolved_path}' (use_tmp_file false)
 
                 """
@@ -171,7 +237,7 @@ class DataAsset:
 
         else:
             print(
-                f"No artifacts have been found for asset_id: {self.id}\n When using glob pattern: {glob_pattern}\n While attempting to write to {self.resolved_path}"
+                f"No artifacts have been found for asset_id: {self.id}\n When using artifact glob: {artifact_glob}\n While attempting to write to {self.resolved_path}"
             )
 
     async def _handle_return(self, *args, **kwargs):
@@ -179,21 +245,48 @@ class DataAsset:
         output = await self.__fn(*args, **kwargs)
 
         # If nothing is return do not perform write operation
-        if self._output_has_data(output):
+        if _output_has_data(output):
             # Write output file to provided path
-            await self._write_operation(self.resolved_path, output)
+            await _write_asset_data(self.resolved_path, output)
 
             # TODO: Normalize artifact write process to align with _handle_yield
             # Write raw json file to appropriate location
-            if ".json" not in self.resolved_path:
-                base_path = await self._get_artifact_base_path()
-                file_name = await self._get_file_name()
-                artifact_path = (
-                    f"{base_path}/_artifact/runtime={self.runtime_str}/{file_name}.json"
-                )
-                await self._write_operation(artifact_path, output)
+            artifact = await self._handle_artifact(output)
+            if artifact.data_written:
+                self.data_written = True
+                self.artifact_glob = artifact.path
 
-            self.data_written = True
+    async def _handle_artifact(
+        self, output: object, fragment_number: int | None = None
+    ):
+        if isinstance(output, DataAssetArtifact):
+            # If the output is already a DataAssetArtifact
+            # use the input artifact_dir as base_path & httpx.Response params as params
+            artifact = output
+            params = output._get_params()
+            base_path = output._get_base_path()
+        else:
+            # If the output isn't a DataAssetArtifact manually set the params & base_path
+            # and initialize the output as a DataAssetArtifact
+            params = (
+                dict(output.request.url.params)
+                if isinstance(output, httpx.Response) and output.request.url.params
+                else None
+            )
+            base_path = self._get_artifact_base_path()
+            artifact = DataAssetArtifact(artifact=output, dir=base_path)
+
+        path = self._build_artifact_path(base_path, params, fragment_number)
+
+        # This function will record artifact metadata and write the artifact to path
+        await artifact._run_artifact(
+            asset_id=self.id,
+            asset_run_id=self.run_id,
+            asset_name=self.name,
+            artifact_path=path,
+        )
+
+        return artifact
 
     def _build_artifact_path(
         self,
@@ -201,58 +294,35 @@ class DataAsset:
         params: dict | None = None,
         fragment_number: int | None = None,
     ):
+        asset_runtime_str = (
+            f"runtime={self.runtime_str}/" if self.snapshot_artifacts else ""
+        )
+
+        if params is None and fragment_number is None:
+            filename = self._get_filename()
+            return f"{base_path}/{asset_runtime_str}{filename}.json"
 
         if params is None:
-            return f"{base_path}/_artifacts/runtime={self.runtime_str}/fragment={fragment_number}.json"
+            return f"{base_path}/{asset_runtime_str}fragment={fragment_number}.json"
 
         params_path = "/".join(f"{key}={value}" for key, value in params.items())
 
-        return f"{base_path}/_artifacts/runtime={self.runtime_str}/{params_path}.json"
+        return f"{base_path}/{asset_runtime_str}{params_path}.json"
 
-    async def _get_artifact_base_path(self):
+    def _get_artifact_base_path(self):
         # Extract folder path for folder set up
-        folder_path = await self._get_folder_path(self.resolved_path)
+        folder_path = os.path.dirname(self.resolved_path)
 
         # Set up the base path for artifact storage
         if not self.resolved_artifacts_dir:
-            base_path = folder_path
+            base_path: str = f"{folder_path}/_artifacts"
         else:
-            base_path = self.resolved_artifacts_dir
+            base_path: str = self.resolved_artifacts_dir
 
         return base_path
 
-    async def _write_operation(self, path: str, data: object):
-        fs = await get_fs()
-        await register_mad_protocol()
-
-        if isinstance(data, (duckdb.DuckDBPyRelation, pandas.DataFrame)):
-            # Before using COPY TO statement ensure directory exists
-            folder_path = await self._get_folder_path(path)
-            fs.mkdirs(folder_path, exist_ok=True)
-
-            json_format = "FORMAT JSON, ARRAY true," if ".json" in path else ""
-            duckdb.query("SET temp_directory = './.tmp/duckdb/'")
-            duckdb.query(
-                f"""
-                    COPY(
-                        SELECT * FROM data
-                    ) TO 'mad://{path}' ({json_format} use_tmp_file false)
-                """
-            )
-        elif isinstance(data, httpx.Response):
-            # TODO: Find way to process raw text responses
-            await fs.write_data(path, data.json())
-        else:
-            await fs.write_data(path, data)
-
-    async def _get_folder_path(self, path: str):
-        fs = await get_fs()
-        folder_path = re.sub(r"[^/\\]+$", "", path)
-
-        return folder_path
-
-    async def _get_file_name(self):
-        return re.search(r"([^\\/]+)(?=\.[^\\/\.]+$)", self.resolved_path).group(1)
+    def _get_filename(self):
+        return os.path.splitext(os.path.basename(self.resolved_path))[0]
 
     def _resolve_attribute(self, input_str: str = ""):
         if not input_str:
@@ -274,22 +344,6 @@ class DataAsset:
         hash_input = f"{self.resolved_name}:{self.resolved_path}:{self.resolved_artifacts_dir}:{self.runtime_str}:{str(self.args)}"
         return hashlib.md5(hash_input.encode()).hexdigest()
 
-    def _output_has_data(self, data: object):
-        if isinstance(data, httpx.Response):
-            # TODO: Create checking mechanism for raw text responses
-            check_result = True if data.json() else False
-
-        elif isinstance(data, duckdb.DuckDBPyRelation):
-            check_result = True if len(data.df()) else False
-
-        elif isinstance(data, pandas.DataFrame):
-            check_result = True if len(data) else False
-
-        else:
-            check_result = True if data else False
-
-        return check_result
-
     async def __register_asset(self):
         fs = await get_fs()
         asset_metadata = {
@@ -305,7 +359,7 @@ class DataAsset:
         }
 
         await fs.write_data(
-            f"{self.ASSET_METADATA_LOCATION}/asset_name={self.resolved_name}/asset_id={self.id}/{self.run_id}.json",
+            f"{ASSET_METADATA_LOCATION}/asset_name={self.resolved_name}/asset_id={self.id}/asset_run_id={self.run_id}/metadata.json",
             asset_metadata,
         )
 
@@ -314,6 +368,48 @@ class DataAsset:
 
     def __setstate__(self, query: str):
         pass
+
+
+def _output_has_data(data: object):
+    if isinstance(data, httpx.Response):
+        # TODO: Create checking mechanism for raw text responses
+        check_result = True if data.json() else False
+
+    elif isinstance(data, duckdb.DuckDBPyRelation):
+        check_result = True if len(data.df()) else False
+
+    elif isinstance(data, pandas.DataFrame):
+        check_result = True if len(data) else False
+
+    else:
+        check_result = True if data else False
+
+    return check_result
+
+
+async def _write_asset_data(path: str, data: object):
+    fs = await get_fs()
+    await register_mad_protocol()
+
+    if isinstance(data, (duckdb.DuckDBPyRelation, pandas.DataFrame)):
+        # Before using COPY TO statement ensure directory exists
+        folder_path = os.path.dirname(path)
+        fs.mkdirs(folder_path, exist_ok=True)
+
+        json_format = "FORMAT JSON, ARRAY true," if ".json" in path else ""
+        duckdb.query("SET temp_directory = './.tmp/duckdb/'")
+        duckdb.query(
+            f"""
+                COPY(
+                    SELECT * FROM data
+                ) TO 'mad://{path}' ({json_format} use_tmp_file false)
+            """
+        )
+    elif isinstance(data, httpx.Response):
+        # TODO: Find way to process raw text responses
+        await fs.write_data(path, data.json())
+    else:
+        await fs.write_data(path, data)
 
 
 async def get_asset_metadata():
