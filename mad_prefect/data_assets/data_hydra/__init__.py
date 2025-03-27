f8a89e4174767728630f2946fbf55d24624d8f6c
Maitland Marshall <maitland@mait.dev>
2025-03-27 11:36:52 +1000

    refactor: simplify DataHydra initialization and introduce DataHydraModule for dependency injection

--- mad_prefect/data_assets/data_hydra/__init__.py
+++ mad_prefect/data_assets/data_hydra/__init__.py
@@ -1,49 +1,29 @@
-from mad_prefect.data_assets.options import DataHydraOptions
+from functools import cached_property
 from typing import TypeVar
-from injector import ClassProvider, Injector
+from injector import Injector
 from typing import Generic
 
 T = TypeVar("T")
 
 
 class DataHydra(Generic[T]):
+    from mad_prefect.data_assets.options import DataHydraOptions
+
     def __init__(self, cls: type[T], options: DataHydraOptions):
-        from .data_hydra_head import DataHydraHead
-        from .data_hydra_runner import (
-            DataHydraRun,
-            DataHydraRunner,
-        )
+        from mad_prefect.data_assets.data_hydra.data_hydra_module import DataHydraModule
 
         self.cls = cls
-        self._options = options
-        self._scope = Injector()
+        self.options = options
+        self.scope = Injector(DataHydraModule(self))
 
-        # Register the cls class so that we can inject it later
-        # which will allow us to inject dependencies into the instance of the cls
-        self._scope.binder.bind(
-            self.cls,
-            to=ClassProvider(self.cls),
-        )
-
-        # Make it so object can be used as an alias to inject the instance
-        self._scope.binder.bind(object, to=ClassProvider(self.cls))
-
-        # Register the DataHydra so we can inject it later
-        self._scope.binder.bind(DataHydra, to=self)
-        self._scope.binder.bind(DataHydraHead)
-        self._scope.binder.bind(DataHydraRunner)
-        self._scope.binder.bind(DataHydraRun)
-
-        # Provide default options that will be overridden potentially
-        # from the user via the asset decorator
-        self._scope.binder.bind(DataHydraOptions, to=options)
-
-    def to_runner(self):
+    @cached_property
+    def runner(self):
         from .data_hydra_runner import (
             DataHydraRunner,
         )
 
-        return self._scope.get(DataHydraRunner)
+        # We have one instance of DataHydraRunner per DataHydra
+        return self.scope.get(DataHydraRunner)
 
     def __call__(self):
-        return self.to_runner().run()
+        return self.runner.run()
---
+++ mad_prefect/data_assets/data_hydra/data_hydra_module.py
@@ -0,0 +1,37 @@
+from injector import Binder, ClassProvider, Module
+
+
+class DataHydraModule(Module):
+    from mad_prefect.data_assets.data_hydra import DataHydra
+
+    def __init__(self, hydra: DataHydra):
+        self.hydra = hydra
+
+    def configure(self, binder: Binder):
+        from mad_prefect.data_assets.data_hydra import DataHydra
+        from .data_hydra_head import DataHydraHead
+        from .data_hydra_runner import (
+            DataHydraRun,
+            DataHydraRunner,
+        )
+        from mad_prefect.data_assets.options import DataHydraOptions
+
+        # Register the cls class so that we can inject it later
+        # which will allow us to inject dependencies into the instance of the cls
+        binder.bind(
+            self.hydra.cls,
+            to=ClassProvider(self.hydra.cls),
+        )
+
+        # Make it so object can be used as an alias to inject the instance
+        binder.bind(object, to=ClassProvider(self.hydra.cls))
+
+        # Register the DataHydra so we can inject it later
+        binder.bind(DataHydra, to=self.hydra)
+        binder.bind(DataHydraHead)
+        binder.bind(DataHydraRunner)
+        binder.bind(DataHydraRun)
+
+        # Provide default options that will be overridden potentially
+        # from the user via the asset decorator
+        binder.bind(DataHydraOptions, to=self.hydra.options)
